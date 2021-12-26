2046115f4ab4637a93f0c1fc3de4cb4b78805336
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-26 14:11:48 +0700

    add login.js

---
+++ LogIn.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Text, Button } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+
+
+const LogIn =() => {
+        return(
+        <SafeAreaView>
+            <Text>LogIn</Text>
+            <Text>Doesnt have account yet?</Text>
+
+            <Button title='Register'></Button>
+        </SafeAreaView>
+        );
+    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         username: '',
+    //         password: ''
+    //     }
+    // }
+
+    // handleUserChange = (event) => {
+    //     this.setState({
+    //         username: event.target.value
+    //     });
+    // }
+
+    // handlePassChange = (event) => {
+    //     this.setState({
+    //         password: event.target.value
+    //     });
+    // }
+
+    // handleLogin = (event) => {
+    //     event.preventDefault();
+    //     this.props.handleLogin(this.state.username, this.state.password);
+    // }
+    
+export default LogIn;
