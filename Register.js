bd3fc4b294f739d8f5698dc93890f66ffa0bb41c
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-27 19:15:04 +0700

    working on Register

---
+++ Register.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { SafeAreaView } from 'react-native';
+import { Dimensions } from 'react-native';
+import { Text,TextInput,StyleSheet } from 'react-native';
+import { Platform } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const Register = () => {
+    return(
+        <SafeAreaView style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+            <Text style={styles.textregister}>Register</Text>
+            <TextInput style={styles.textinput} placeholderTextColor='blue' placeholder="Username"/>
+            <TextInput style={styles.textinput} placeholder="Password" />
+            <TextInput style={styles.textinput} placeholder="Confirm Password" />
+            <TextInput style={styles.textinput} placeholder="Email" />
+        </SafeAreaView>
+    );
+}
+const styles = StyleSheet.create({
+    textregister: {
+        fontSize: 30,
+        ...Platform.select({
+        android: {
+           fontFamily: 'sans-serif-medium',
+         },
+        ios: {
+            fontFamily: 'AppleSDGothicNeo-Bold',
+        },
+        default: {
+        // other platforms, web for example
+           fontFamily: 'Arial',
+        },
+        }),
+        marginBottom: height/4,
+    },
+    textinput: {
+        width: width - 60,
+        height: height/15,
+        borderColor: 'black',
+        borderWidth: 2,
+        alignSelf: 'center',
+        marginBottom: 20,
+        borderRadius: 20,
+    },
+    placeholderstyle: {
+        alignSelf: 'center',
+        fontSize: 15,
+        color: 'blue',
+        marginBottom: 10,
+    }
+});
+export default Register;
