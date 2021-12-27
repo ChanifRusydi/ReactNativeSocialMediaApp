d9318eb49f2d3ff636a9ff112b606396c2d3813c
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-27 15:26:15 +0700

    working on FormAdd

--- FormAdd.js
+++ FormAdd.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import {View, Text, Button,Image} from 'react-native';
+import {View, Text, Button, Image, TextInput,StyleSheet} from 'react-native';
 
-export const FormAdd = ({ routte,navigation }) => {
+export const FormAdd = ({ route,navigation }) => {
     const[username, setUsername] = useState('');
     cosnt[textInputPost, setTextInputPost] = useState('')
 
@@ -20,5 +20,40 @@ export const FormAdd = ({ routte,navigation }) => {
             });
             const json = await response.json();
             setData(json.data);
+        }catch(error){
+            console.log(error);
+        }finally{
+            navigation.navigate
         }
-        
+
+        return (
+            <View style={{flex:1, padding:4, backgroundColor:'black'}}>
+                <Text style={{color:'white'}}>{item.userId}</Text>
+                <TextInput placeholder='tulis post anda'  style={StyleSheet.input} onChangeText={(text) => setTextInputPost(text)}/>
+
+        )
+    
+    
+    
+const styles = StyleSheet.create({
+    input: {
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+        fontSize: 18,
+        color: 'black'
+    },
+    button: {
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+        fontSize: 18,
+        color: 'black'
+    }
+});
+
+export default FormAdd;
