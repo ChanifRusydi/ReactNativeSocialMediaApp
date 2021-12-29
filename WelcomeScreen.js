58c275fb925ad29dcc45c1dcf6a47ee03299c3ad
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-29 21:27:17 +0700

    working on welcome screen

---
+++ WelcomeScreen.js
@@ -0,0 +1,44 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, Button, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+import {LogIn as LogInScreen} from './LogIn';
+import {Register as RegisterScreen} from './Register';
+
+
+const WelcomeScreen = ({ navigation }) => {
+    return (
+        <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Welcome To our Social Media App</Text>
+            <Button title='Log In' onPress={() => navigation.navigate('LogIn')} />
+            <Button title='Sign Up' onPress={() => navigation.navigate('Register')} />
+        </SafeAreaView>
+    );
+}
+const stack = createNativeStackNavigator();
+
+const WelcomeStack = () => {
+    return (
+     <NavigationContainer>
+        <stack.Navigator>
+            <stack.Screen name='Welcome' component={WelcomeScreen} options={{ headerShown: false }} />
+            <stack.Screen name='LogIn' component={LogInScreen} options={{ headerShown: false }} />
+            <stack.Screen name='Register' component={RegisterScreen} options={{ headerShown: false }} />
+        </stack.Navigator>
+    </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({
+    welcome:  {
+        fontSize: 30,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 10
+    },
+    button; {
+        backgroundColor: '#20F4C4'
+    }
+});
+export default WelcomeStack;
+
