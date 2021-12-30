0b757843e7074f51f62b13d2e64c39b6d118592c
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-30 20:58:44 +0700

    add Search Notifications Profile

--- SocialMediaApp.js
+++ SocialMediaApp.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { PostList as PostListScreen } from './PostList';
-import { Search as SearchScreen} from './SearchScreen';
-import { Notifications as NotificationsScreen} from './NotificationsScreen';
+import { PostList as PostListTab} from './PostList';
+import { Search as SearchTab} from './SearchScreen';
+import { Notifications as NotificationTab} from './NotificationsScreen';
 import { Profile as ProfileTab} from './ProfileScreen';
 
 const Tab = createBottomTabNavigator();
@@ -11,9 +11,9 @@ const AppHome = () => {
     return (  
         <NavigationContainer>
             <Tab.Navigator initialRouteName>
-                <Tab.Screen name="Home" component={PostListScreen} />
-                <Tab.Screen name="Search" component={SearchScreen} />
-                <Tab.Screen name="Notifications" component={NotificationsScreen} />
+                <Tab.Screen name="Home" component={PostListTab} />
+                <Tab.Screen name="Search" component={SearchTab} />
+                <Tab.Screen name="Notifications" component={NotificationsTab} />
                 <Tab.Screen name="Profile" component={ProfileTab} />
             </Tab.Navigator>
         </NavigationContainer>
