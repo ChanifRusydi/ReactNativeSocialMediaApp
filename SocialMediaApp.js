5369b2f0c3a882d6a3385970c7e36c073cc93c2e
ChanifRusydi <chanif.rusydi@gmail.com>
2021-12-30 20:36:48 +0700

    add screen

--- SocialMediaApp.js
+++ SocialMediaApp.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { PostList as PostListScreen } from './PostList';
+import { Search as SearchScreen} from './SearchScreen';
+import { Notifications as NotificationsScreen} from './NotificationsScreen';
+import { Profile as ProfileTab} from './ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 const AppHome = () => {
@@ -11,7 +14,7 @@ const AppHome = () => {
                 <Tab.Screen name="Home" component={PostListScreen} />
                 <Tab.Screen name="Search" component={SearchScreen} />
                 <Tab.Screen name="Notifications" component={NotificationsScreen} />
-                <Tab.Screen name="Profile" component={ProfileScreen} />
+                <Tab.Screen name="Profile" component={ProfileTab} />
             </Tab.Navigator>
         </NavigationContainer>
 
